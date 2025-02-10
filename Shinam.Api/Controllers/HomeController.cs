83f86bcdc27995958590d7761eeb31f5048c0996
Foziljon06 <fozilxolmatov9@gmail.com>
2025-02-10 22:47:30 +0300

    second times

---
+++ Shinam.Api/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿//===============================
+// bu Faylda file header yaratdim
+// negaligini hozircha bilmayaman
+//===============================
+
+
+using Microsoft.AspNetCore.Mvc;
+using RESTFulSense.Controllers;
+
+namespace Shinam.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HomeController : RESTFulController
+    {
+        [HttpGet]
+        public ActionResult<string> Get() =>
+            Ok("Hello How are you");
+    }
+}
---
+++ Shinam.Api/Program.cs
@@ -0,0 +1,27 @@
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+
+// Swagger sozlamalari
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Shinam.Api", Version = "v1" });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Shinam.Api v1"));
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
