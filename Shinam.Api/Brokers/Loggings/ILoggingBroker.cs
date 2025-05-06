0f99312efd87bbc6760c083b5cd52164b6409955
Foziljon06 <fozilxolmatov9@gmail.com>
2025-05-06 06:33:23 +0300

    brikersga: Logging briker va IloggingBroker qo`shildi va program.cs  ga implimentatsiya qilindi

---
+++ Shinam.Api/Brokers/Loggings/ILoggingBroker.cs
@@ -0,0 +1,13 @@
+﻿//===============================
+// bu Faylda file header yaratdim
+// negaligini hozircha bilmayaman
+//===============================
+
+namespace Shinam.Api.Brokers.Loggings
+{
+    public interface ILoggingBroker
+    {
+        void LogError(Exception exeption);
+        void LogCritical(Exception exception);
+    }
+}
---
+++ Shinam.Api/Brokers/Loggings/LoggingBroker.cs
@@ -0,0 +1,24 @@
+﻿//===============================
+// bu Faylda file header yaratdim
+// negaligini hozircha bilmayaman
+//===============================
+
+
+using System;
+
+namespace Shinam.Api.Brokers.Loggings
+{
+    public class LoggingBroker : ILoggingBroker
+    {
+        private readonly ILogger<LoggingBroker> logger;
+
+        public LoggingBroker(ILogger<LoggingBroker> logger) => this.logger = logger;
+
+        public void LogError(Exception exception) =>
+        this.logger.LogError(exception, exception.Message);
+
+        public void LogCritical(Exception exception) =>
+            this.logger.LogCritical(exception, exception.Message);
+           
+    }
+}
